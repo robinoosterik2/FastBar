421e49b351519d955642359fa971a94b24c41a4c
Robin Oosterik <robin.oosterik@pescheck.nl>
2025-04-02 23:32:29 +0200

    fixed button tests

--- test/components/button_component_test.rb
+++ test/components/button_component_test.rb
@@ -5,17 +5,17 @@ require "test_helper"
 class ButtonComponentTest < ViewComponent::TestCase
   def test_renders_primary_button
     render_inline(ButtonComponent.new(text: "Click Me", variant: :primary, path: "/test_path"))
-    assert_selector "a.bg-blue-600.text-white.hover\\:bg-blue-700", text: "Click Me"
+    assert_selector "a.bg-primary", text: "Click Me"
   end
 
   def test_renders_secondary_button
     render_inline(ButtonComponent.new(text: "Click Me", variant: :secondary, path: "/test_path"))
-    assert_selector "a.bg-gray-200.text-gray-800.hover\\:bg-gray-300", text: "Click Me"
+    assert_selector "a.bg-secondary", text: "Click Me"
   end
 
   def test_defaults_to_primary_variant
     render_inline(ButtonComponent.new(text: "Click Me", path: "/test_path"))
-    assert_selector "a.bg-blue-600.text-white.hover\\:bg-blue-700", text: "Click Me"
+    assert_selector "a.bg-primary", text: "Click Me"
   end
 
   def test_renders_with_custom_path
@@ -25,6 +25,6 @@ class ButtonComponentTest < ViewComponent::TestCase
 
   def test_invalid_variant_defaults_to_primary
     render_inline(ButtonComponent.new(text: "Click Me", variant: :invalid, path: "/test_path"))
-    assert_selector "a.bg-blue-600.text-white.hover\\:bg-blue-700", text: "Click Me"
+    assert_selector "a.bg-primary", text: "Click Me"
   end
 end
