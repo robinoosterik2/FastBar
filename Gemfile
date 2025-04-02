29f4eea8754a0005e1811259e7a0c4ba597ee8f9
Robin Oosterik <robin.oosterik@pescheck.nl>
2025-04-02 14:50:45 +0200

    started building button component

--- Gemfile
+++ Gemfile
@@ -65,3 +65,4 @@ end
 gem "tailwindcss-ruby", "~> 4.0"
 
 gem "tailwindcss-rails", "~> 4.2"
+gem "view_component"
---
+++ app/components/button_component.rb
@@ -0,0 +1,19 @@
+class ButtonComponent < ViewComponent::Base
+	ALLOWED_VARIANTS = %i[primary secondary].freeze
+
+	attr_reader :text, :variant
+
+	def initialize(text:, variant: :primary)
+		@text = text
+		@variant = ALLOWED_VARIANTS.include?(variant.to_sym) ? variant.to_sym : :primary
+	end
+
+	def classes
+		base = "mx-2 px-4 rounded font-semibold focus:outline-none"
+		variant_classes = {
+			primary: "bg-blue-600 text-white hover:bg-blue-700",
+			secondary: "bg-gray-200 text-gray-800 hover:bg-gray-300"
+		}
+		"#{base} #{variant_classes[variant]}"
+	end
+end
