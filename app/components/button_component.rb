5192e2c666acced35409df37780af2d2047210a6
Robin Oosterik <robin.oosterik@pescheck.nl>
2025-04-02 20:31:03 +0200

    added theme colors and updated button style

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -1,20 +1,32 @@
 class ButtonComponent < ViewComponent::Base
-  ALLOWED_VARIANTS = %i[primary secondary].freeze
+  ALLOWED_VARIANTS = %i[primary secondary warning error].freeze
+  ALLOWED_ICON_POSITIONS = %i[start end none].freeze
 
-  attr_reader :text, :variant, :path
+  attr_reader :text, :variant, :path, :icon, :icon_position
 
-  def initialize(text:, variant: :primary, path: "#")
+  def initialize(text:, variant: :primary, path: "#", icon: nil, icon_position: :none)
     @text = text
     @variant = ALLOWED_VARIANTS.include?(variant.to_sym) ? variant.to_sym : :primary
     @path = path
+    @icon = icon
+    @icon_position = ALLOWED_ICON_POSITIONS.include?(icon_position.to_sym) ? icon_position.to_sym : :none
   end
 
   def classes
-    base = "mx-2 px-4 rounded font-semibold focus:outline-none"
+    base = "mx-2 px-4 rounded font-semibold focus:outline-none flex items-center justify-center hover:scale-105 text-text transition-all duration-200 ease-in-out hover:shadow-lg relative"
     variant_classes = {
-      primary: "bg-blue-600 text-white hover:bg-blue-700",
-      secondary: "bg-gray-200 text-gray-800 hover:bg-gray-300"
+      primary: "bg-primary",
+      secondary: "bg-secondary",
+      warning: "bg-warning",
+      error: "bg-error"
     }
     "#{base} #{variant_classes[variant]}"
+  end
+
+  def render_icon
+    return nil unless icon && icon_position != :none
+
+    position_class = icon_position == :start ? "left-4" : "right-4"
+    "<i class=\"#{icon} #{position_class} absolute\"></i>".html_safe
   end
 end
--- notes/gems.md
+++ notes/gems.md
@@ -1,2 +1,4 @@
 [gem install annotaterb](https://github.com/drwl/annotaterb) <!-- will add comments about the model -->
 [tailwind](https://tailwindcss.com/docs/installation/framework-guides/ruby-on-rails)
+
+gem "view_component"
