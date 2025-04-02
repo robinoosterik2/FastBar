bef9a01d2aa3790fa4e3ad6ace1abc7928a26082
Robin Oosterik <robin.oosterik@pescheck.nl>
2025-04-02 17:23:41 +0200

    added working button

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
-    
+  end
+  def log_click
+    Rails.logger.info("🚀 Button clicked at #{Time.current}")
+    head :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   root "pages#home"
+  post "log_click", to: "pages#log_click", as: :log_click
 end
